40e43da1e2e7ce22150ad6cae69a0880fc796f32
Siddhant Shukla <9d32.siddhantshukla@gmail.com>
2026-07-10 00:27:28 +0530

    changing base_url for deploying

--- planetvault-frontend/src/services/api.js
+++ planetvault-frontend/src/services/api.js
@@ -1,6 +1,5 @@
 
-
-const BASE_URL="http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_API_URL;
 export async function signup(userData){
     const response = await fetch(`${BASE_URL}/signup` , {
         method:'POST',
