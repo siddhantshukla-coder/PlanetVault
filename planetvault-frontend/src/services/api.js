2af3d382dc63a162b114b5b6e7e50880d4ce06b5
Siddhant Shukla <9d32.siddhantshukla@gmail.com>
2026-07-10 01:02:05 +0530

    small bug fix

--- planetvault-frontend/src/services/api.js
+++ planetvault-frontend/src/services/api.js
@@ -1,5 +1,6 @@
 
 const BASE_URL = import.meta.env.VITE_API_URL;
+
 export async function signup(userData){
     const response = await fetch(`${BASE_URL}/signup` , {
         method:'POST',
@@ -8,7 +9,7 @@ export async function signup(userData){
         },
         body:JSON.stringify(userData)
     });
-    const data=response.json();
+    const data=await response.json();
     if(!response.ok){
         throw new Error(data.message);
     }
